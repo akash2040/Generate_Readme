dbff5fc025d07c7459629a90798ca7e4866cc90d
Akash Chanara <akashchanara@gmail.com>
2022-10-21 00:21:36 -0700

    compelete project

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Generate_Readme
+
+## User Story
+
+AS A developer I WANT a README generator SO THAT can quickly create a professional README for a new project
+
+## Summary
+
+This application uses node.js, fs, util and inquirer to create prompts for the user to fill out and then construct readme markdown document and have the prompt fill in the document.
+
+## Installation:
+
+You need to make sure node.js, util, fs and inquirer instaled before you run the application.
+
+```Code
+npm init -y
+npm i inquirer@8.2.4
+```
+
+## Usage
+
+This application allows for a user to create a readme file.
+
+## Project Demo
+
+![pic](./output/video1%20.gif)
+
+## Github[https://github.com/akash2040/Generate_Readme]
--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  `Enter project name: `,
+  `Enter description: `,
+  `Enter Installation: `,
+  `Enter Usage: `,
+  `Enter License: `,
+  `Enter Contributing:`,
+  `Enter Tests: `,
+  `Enter Github: `,
+  `Enter EMAIL: `,
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data, callback) {
+  fs.writeFile("./output/README.md", data, callback);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      { type: "input", message: questions[0], name: "title" },
+      { type: "input", message: questions[1], name: "description" },
+      { type: "input", message: questions[2], name: "installation" },
+      { type: "input", message: questions[3], name: "usage" },
+      {
+        type: "list",
+        message: questions[4],
+        name: "license",
+        choices: ["Apache", "IBM", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+      },
+      { type: "input", message: questions[5], name: "Contribution" },
+      { type: "input", message: questions[6], name: "Tests" },
+      { type: "input", message: questions[7], name: "username" },
+      { type: "input", message: questions[8], name: "email" },
+    ])
+    .then((data) => {
+      // console.log(data);
+      writeToFile("./output/README.md", generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log("Created!")
+      );
+    })
+    .catch((error) => console.log(error));
+}
 
 // Function call to initialize app
 init();
